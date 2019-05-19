fa8f33c684bf337dd027ca957e7c411e9a08c1fd
ihnokim <ihnokim58@gmail.com>
2019-05-20 01:08:54 +0900

    remove unnecessary codes

--- radix_sort.cpp
+++ radix_sort.cpp
@@ -1,17 +1,16 @@
 #include <malloc.h>
-
 const int P = (1 << 8);
-extern void radix_sort(int n, int* a)
+int cnt[P];
+
+extern void radix_sort(int n, int* a, int* tmp)
 {
-	int *b = (int*)malloc(n * sizeof(int)), cnt[P];
-	register int i;
-	for (int k = 0; k < 32; k += 8)
+	register int i, k;
+	for (k = 0; k < 32; k += 8)
 	{
 		for (i = 0; i < P; i++) cnt[i] = 0;
 		for (i = 0; i < n; i++) cnt[(a[i] >> k) & (P - 1)]++;
 		for (i = 1; i < P; i++) cnt[i] += cnt[i - 1];
-		for (i = n - 1; i >= 0; i--) b[--cnt[(a[i] >> k) & (P - 1)]] = a[i];
-		int* tmp = a; a = b; b = tmp;
+		for (i = n - 1; i >= 0; i--) tmp[--cnt[(a[i] >> k) & (P - 1)]] = a[i];
+		int* s = a; a = tmp; tmp = s;
 	}
-	free(b);
 }
